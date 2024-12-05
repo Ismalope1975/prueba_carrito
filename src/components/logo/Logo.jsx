60df5a27bec86d355315711b0ca749ff6fbc0a9f
Ismael Lopez <ismael_lopez83@hotmail.com>
2024-12-05 12:18:06 -0300

    logo ruta

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo() {
   return (
     <img
-    src="../public/img/designer.png"
+    src="/public/img/designer.png"
     alt="Logo"
     width="100"
     height="100"
